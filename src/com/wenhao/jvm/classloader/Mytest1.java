4f9a3be22b5b8cc579ac79861e71bc5ab4fbc45b
wenhao <921790476@qq.com>
2020-02-23 21:56:55 +0800

    edit

--- src/com/wenhao/jvm/classloader/Mytest1.java
+++ src/com/wenhao/jvm/classloader/Mytest1.java
@@ -2,7 +2,7 @@ package com.wenhao.jvm.classloader;
 
 /**
  * 对于静态字段来说，只有直接定义了该字段的类才会被初始化
- * 当一个类在初始时，要求其父类要全部初始化完毕了，子类才初始化
+ * 当一个类在初始化时，要求其父类要全部初始化完毕了，子类才初始化
  */
 public class Mytest1 {
     public static void main(String[] args) {
