5d6da7a0db62e8e79036014fc3b899dc17cc1d9c
wenhao <921790476@qq.com>
2020-02-25 00:03:47 +0800

    edit

---
+++ src/com/wenhao/jvm/classloader/MyTest2.java
@@ -0,0 +1,35 @@
+package com.wenhao.jvm.classloader;
+
+/**
+ * 常量在编译阶段会存入到调用这个常量的方法所在的类的常量池中
+ * 本质上，调用类并没有直接引用到定义常量的类，因此并不会触发
+ * 定义常量的类的初始化
+ * 注意：这里指的事将常量存放到了MyTest2的常量池中之后MyTest2与MyParent2
+ * 就没有任何关系了。
+ * 甚至，我们可以将MyParent2的class文件删除
+ *
+ * 助记符：
+ * ldc 表示将int，float或是String 类型的常量值从常量池中推送至栈顶
+ * bipush 表示将单字节 (-128~ 127)的常量值推送至栈顶
+ * sipush 表示将短整型常量值 (-32768~ 32767)推送至栈顶
+ * iconst_1 表示将int类型 推送至栈顶 (iconst_1 - iconst_5) 6就会变成 bipush
+ */
+public class MyTest2 {
+    public static void main(String[] args) {
+        System.out.println(MyParent2.str);
+    }
+}
+
+class MyParent2{
+//    public static String str = "hello world";
+    public static final String str = "hello world";
+    public static final short s = 128;
+    public static final int i = 128;
+    public static final int m = 1;
+//    public static final int m = 2; 明天测试
+//    public static final int m = 3;
+
+    static {
+        System.out.println("MyParent2 static block");
+    }
+}
